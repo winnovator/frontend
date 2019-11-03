17590b012408106b07e684216a1e37d6a9c14c7d
Gert Stoevelaar <51631119+stoevie@users.noreply.github.com>
2019-11-03 22:11:28 +0100

    d

--- karma.conf.js
+++ karma.conf.js
@@ -23,7 +23,7 @@ module.exports = function (config) {
         '--headless',
         '--disable-gpu',
         '--no-sandbox',
-        '--remote-debugging-port=9222',
+        '--remote-debugging-port=9222'
         ]
       }
     },
