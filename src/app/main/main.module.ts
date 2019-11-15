add45a4136e09f7386117a10a0245887ae0d2ace
Gert Stoevelaar <51631119+stoevie@users.noreply.github.com>
2019-11-15 18:24:27 +0100

    QRcode uitlezen toegevoegd aan main page

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MainPage } from './main.page';
+import { ZXingScannerModule } from '@innotec/ngx-scanner';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    ZXingScannerModule,
     RouterModule.forChild([{ path: '', component: MainPage }])
   ],
   declarations: [MainPage]
--- src/app/main/main.page.ts
+++ src/app/main/main.page.ts
@@ -3,8 +3,9 @@ import { TokenApiService } from './../services/token-api.service';
 import { TabsService } from './../tabs/tabs.service';
 import { DesignShop } from '../models/designshop';
 import { DesignshopApiService } from '../services/designshop-api.service';
-import { Component,  OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { TabsPage } from '../tabs/tabs.page';
+import { ZXingScannerComponent } from '@innotec/ngx-scanner';
 
 
 
@@ -17,22 +18,72 @@ import { TabsPage } from '../tabs/tabs.page';
 
 
 export class MainPage implements OnInit {
+
+  @ViewChild('scan', { static: true, read: false}) scanner: ZXingScannerComponent;
+
+  hasCameras = false;
+  hasPermission: boolean;
+  qrResultString: string;
+
+  availableDevices: MediaDeviceInfo[];
+  selectedDevice: MediaDeviceInfo;
+
   designshops: DesignShop[];
   designshop: DesignShop;
 
   constructor(private designshopApiService: DesignshopApiService, private tabs: TabsPage,
-              private tabsService: TabsService, private tokenApiService: TokenApiService) {}
+              private tabsService: TabsService, private tokenApiService: TokenApiService) { }
 
-  public async ngOnInit() {
-    this.getDesignShops();
+  public ngOnInit(): void {
+
+
+    //this.getDesignShops();
+
+    this.scanner.camerasFound.subscribe((devices: MediaDeviceInfo[]) => {
+    this.hasCameras = true;
+
+      console.log('Devices: ', devices);
+      this.availableDevices = devices;
+
+      // selects the devices's back camera by default
+      // for (const device of devices) {
+      //     if (/back|rear|environment/gi.test(device.label)) {
+      //         this.scanner.changeDevice(device);
+      //         this.selectedDevice = device;
+      //         break;
+      //     }
+      // }
+    });
+
+    this.scanner.camerasNotFound.subscribe((devices: MediaDeviceInfo[]) => {
+      console.error('An error has occurred when trying to enumerate your video-stream-enabled devices.');
+    });
+
+    this.scanner.permissionResponse.subscribe((answer: boolean) => {
+      this.hasPermission = answer;
+    });
   }
+
+  handleQrCodeResult(resultString: string) {
+    console.log('Result: ', resultString);
+    this.qrResultString = resultString;
+    this.selectedDevice = null;
+}
+
+onDeviceSelectChange(selectedValue: string) {
+  this.qrResultString = null;
+    console.log('Selection changed: ', selectedValue);
+    this.selectedDevice = this.scanner.getDeviceById(selectedValue);
+}
 
   public optionsFn(): void {
     if (this.designshop != null) {
-    localStorage.setItem('ds', JSON.stringify(this.designshop));
-    this.tabs.cameraDisabled = this.tabsService.SetCameraPageAccess();
+      localStorage.setItem('ds', JSON.stringify(this.designshop));
+      this.tabs.cameraDisabled = this.tabsService.SetCameraPageAccess();
     }
   }
+
+
 
   public async getDesignShops() {
     await this.tokenApiService.getJWTToken().then(data =>
