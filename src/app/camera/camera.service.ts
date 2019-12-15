7434e6a2e97b0e91879383b2a017ae198a8919ca
Gert Stoevelaar <51631119+stoevie@users.noreply.github.com>
2019-12-15 18:50:39 +0100

    fix build issue met paden

--- src/app/camera/camera.service.ts
+++ src/app/camera/camera.service.ts
@@ -1,7 +1,7 @@
 import { DesignShop } from '../models/designshop';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
+import { environment } from './../../environments/environment';
 import { BehaviorSubject, throwError, Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
